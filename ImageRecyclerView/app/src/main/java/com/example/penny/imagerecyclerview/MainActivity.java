d3a7949f9b214e0b520e37af801bcd860c3bb762
jeongjoonsup <penny4860@gmail.com>
2019-01-26 16:32:22 +0900

    recyclerview 위젯 추가

--- ImageRecyclerView/app/src/main/java/com/example/penny/imagerecyclerview/MainActivity.java
+++ ImageRecyclerView/app/src/main/java/com/example/penny/imagerecyclerview/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.penny.imagerecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final LinkedList<String> mWordList = new LinkedList<>();
+
+    private RecyclerView mRecyclerView;
+    //private WordListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + i);
+        }
+
+        // Activity에 RecyclerView를 표시하는 과정
+        // 1. recycler view.
+        mRecyclerView = findViewById(R.id.recyclerview);
+//        // 2. adapter
+//        mAdapter = new WordListAdapter(this, mWordList);
+//        // 3. Link (view -> adaptor)
+//        mRecyclerView.setAdapter(mAdapter);
+//        // 4. item들이 표시되는 layout 설정
+//        // mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        LinearLayoutManager horizontalLayoutManagaer = new LinearLayoutManager(MainActivity.this,
+//                LinearLayoutManager.HORIZONTAL, false);
+//        mRecyclerView.setLayoutManager(horizontalLayoutManagaer);
+
     }
 }
