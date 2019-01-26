196b36f01e92f5875b64f797d18b1fcd2f9624c4
jeongjoonsup <penny4860@gmail.com>
2019-01-26 14:47:15 +0900

    리스트뷰를 가로방식으로 수정

--- RecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -19,6 +19,7 @@ package com.example.android.recyclerview;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -45,22 +46,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        // Put initial data into the word list.
         for (int i = 0; i < 20; i++) {
             mWordList.addLast("Word " + i);
         }
 
-        // 1. Create recycler view.
+        // Activity에 RecyclerView를 표시하는 과정
+        // 1. recycler view.
         mRecyclerView = findViewById(R.id.recyclerview);
-        // 2. Create an adapter and supply the data to be displayed.
+        // 2. adapter
         mAdapter = new WordListAdapter(this, mWordList);
-        // 3. Connect the adapter with the recycler view.
+        // 3. Link (view -> adaptor)
         mRecyclerView.setAdapter(mAdapter);
-        // Give the recycler view a default layout manager.
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // 4. item들이 표시되는 layout 설정
+        // mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager horizontalLayoutManagaer = new LinearLayoutManager(MainActivity.this,
+                LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(horizontalLayoutManagaer);
+
     }
 
 }
